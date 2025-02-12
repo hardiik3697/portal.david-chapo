92cb836a7d440927b47cfc37d076b6afd7135c98
HardIk Patel <hardiik3697@gmail.com>
2025-02-12 16:35:02 -0500

    update index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,8 +16,19 @@
             <div class="card">
                 <div class="d-flex align-items-start row">
                     <div class="col-md-6 text-center text-md-start order-2 order-md-1">
-                        <div class="card-body pb-0 px-0 pt-2">
-                            <img src="{{ asset('assets/img/illustrations/trophy.png') }}" height="186" class="scaleX-n1-rtl" alt="View Profile" data-app-light-img="illustrations/trophy.png" data-app-dark-img="illustrations/trophy.png">
+                        <div class="d-flex align-items-end row">
+                            <div class="col-md-8 order-1 order-md-2">
+                                <div class="card-body">
+                                    <h4 class="card-title mb-4">Welcome <span class="fw-bold">{{ auth()->user()->name ?? 'John' }}!</span> 🎉</h4>
+                                    <p class="mb-0">You have done good work.</p>
+                                    <p>Check your data and analytics.</p>
+                                </div>
+                            </div>
+                            <div class="col-md-4 text-center text-md-start order-2 order-md-1">
+                                <div class="card-body pb-0 px-0 pt-2">
+                                    <img src="{{ asset('assets/img/illustrations/trophy.png') }}" height="186" class="scaleX-n1-rtl" alt="View Profile" data-app-light-img="illustrations/trophy.png" data-app-dark-img="illustrations/trophy.png">
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="col-md-6 order-1 order-md-2">
@@ -27,32 +38,13 @@
                                 <input type="" class="form-control" id="generate_link">
                                 <label for="generate_link">Link</label>
                             </div>
-                            <a href="javascript:;" id="generateButton" class="btn btn-primary waves-effect waves-light">Generate Link</a>
+                            <a href="javascript:;" id="generateButton" class="btn btn-primary btn-group-justified waves-effect waves-light">Generate Link</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-12 col-xxl-8">
-            <div class="card">
-                <div class="d-flex align-items-end row">
-                    <div class="col-md-6 order-2 order-md-1">
-                        <div class="card-body">
-                            <h4 class="card-title mb-4">Welcome <span class="fw-bold">{{ auth()->user()->name ?? 'John' }}!</span> 🎉</h4>
-                            <p class="mb-0">You have done good work.</p>
-                            <p>Check your data and analytics.</p>
-                            <!-- <a href="javascript:;" class="btn btn-primary waves-effect waves-light">View Profile</a> -->
-                        </div>
-                    </div>
-                    <div class="col-md-6 text-center text-md-end order-1 order-md-2">
-                        <div class="card-body pb-0 px-0 pt-2">
-                            <img src="{{ asset('assets/img/illustrations/illustration-john-light.png') }}" height="186" class="scaleX-n1-rtl" alt="View Profile" data-app-light-img="illustrations/illustration-john-light.png" data-app-dark-img="illustrations/illustration-john-dark.png">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xxl-2 col-sm-6">
+        <div class="col-xxl-3 col-sm-6">
             <div class="card h-100">
                 <div class="card-body">
                     <div class="d-flex justify-content-between align-items-start flex-wrap gap-2">
@@ -74,7 +66,51 @@
                 </div>
             </div>
         </div>
-        <div class="col-xxl-2 col-sm-6">
+        <div class="col-xxl-3 col-sm-6">
+            <div class="card h-100">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-start flex-wrap gap-2">
+                        <div class="avatar">
+                            <div class="avatar-initial bg-label-primary rounded-3">
+                                <i class="ri-shopping-cart-2-line ri-24px"></i>
+                            </div>
+                        </div>
+                        <div class="d-flex align-items-center">
+                            <p class="mb-0 text-success me-1">+22%</p>
+                            <i class="ri-arrow-up-s-line text-success"></i>
+                        </div>
+                    </div>
+                    <div class="card-info mt-5">
+                        <h5 class="mb-1">155k</h5>
+                        <p>Total Paid</p>
+                        <div class="badge bg-label-secondary rounded-pill">Last Day</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xxl-3 col-sm-6">
+            <div class="card h-100">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-start flex-wrap gap-2">
+                        <div class="avatar">
+                            <div class="avatar-initial bg-label-primary rounded-3">
+                                <i class="ri-shopping-cart-2-line ri-24px"></i>
+                            </div>
+                        </div>
+                        <div class="d-flex align-items-center">
+                            <p class="mb-0 text-success me-1">+22%</p>
+                            <i class="ri-arrow-up-s-line text-success"></i>
+                        </div>
+                    </div>
+                    <div class="card-info mt-5">
+                        <h5 class="mb-1">155k</h5>
+                        <p>Total Purchase</p>
+                        <div class="badge bg-label-secondary rounded-pill">Last Days</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xxl-3 col-sm-6">
             <div class="card h-100">
                 <div class="card-body">
                     <div class="d-flex justify-content-between align-items-start flex-wrap gap-2">
